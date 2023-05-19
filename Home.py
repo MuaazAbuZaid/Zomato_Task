8923fd53198da2314fcb187750831c3be509e53d
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2023-05-19 19:55:12 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
+import joblib
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
